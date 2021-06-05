0d5b168038d1f57d84cfa38406318c4c3b533ea3
Andrey <altus01@mail.ru>
2021-06-05 23:51:16 +0400

    profile pages for users

--- src/components/profile/profileInfo/profileInfo.js
+++ src/components/profile/profileInfo/profileInfo.js
@@ -12,17 +12,19 @@ const ProfileInfo = (props) => {
 		)
 	}
 
+	const {photos, name, birthday, city, personalSite, status} = props.profile;
+
 	return (
 		<div className={s.profile_info}>
 			<img src="https://sites.google.com/site/prirodanasevseegooglgfgf/_/rsrc/1463456237313/home/priroda_gory_nebo_ozero_oblaka_81150_1920x1080.jpg" />
 			<div className={s.avatar}>
-				<img src={props.profile.photos.small === null ? photoUserDefault : props.profile.photos.small} />
+				<img src={photos.small === null ? photoUserDefault : photos.small} />
 				<div className={s.avatar_text}>
-					<h1>{props.profile.name}</h1>
-					<p>Date: {props.profile.birthday}</p>
-					<p>City: {props.profile.city}</p>
-					<p>Web site: {props.profile.personalSite}</p>
-					<p>Status: {props.profile.status}</p>
+					<h1>{name}</h1>
+					<p>Date: {birthday}</p>
+					<p>City: {city}</p>
+					<p>Web site: {personalSite}</p>
+					<p>Status: {status}</p>
 				</div>
 			</div>
 		</div>
